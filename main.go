c19612b93dfe9694341725c51fd001e064b4a0e1
FlomoN <florian.giger@vent.io>
2022-10-28 17:25:52 +0200

    chore: first try with kubeapi

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	discovery "github.com/gkarthiks/k8s-discovery"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
 
 func main(){
 	fmt.Println("Hello World");
+
+	k8s, _ := discovery.NewK8s();
+
+	clientSet, err := kubernetes.NewForConfig(k8s.RestConfig);
+	if(err != nil){
+		panic(err.Error())
+	}
+
+	fmt.Println(clientSet.CoreV1().Namespaces().List(context.TODO(), v1.ListOptions{}));
 }
